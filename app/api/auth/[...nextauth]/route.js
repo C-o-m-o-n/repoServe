bc39ed9ec9de5a117655f8ebe3163ce65052433a
Collins O. Odhiambo <comon928@gmail.com>
2023-10-13 14:43:45 +0000

    added nextAuth

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,11 @@
+import nextAuth from 'next-auth'
+import NextAuth from 'next-auth'
+import GitHub from 'next-auth/providers/github'
+import { options } from './options'
+// import Facebook from 'next-auth/providers/facebook'
+// import Google from 'next-auth/providers/google'
+
+
+const Handler = nextAuth(options)
+
+export {Handler as Get, Handler  as Post}
