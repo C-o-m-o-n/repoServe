615cf385812d8d267d2a07870dd8d454bcf6ea07
Collins O. Odhiambo <comon928@gmail.com>
2023-10-14 11:43:10 +0000

    edit client

--- app/client/page.js
+++ app/client/page.js
@@ -6,16 +6,28 @@ import { redirect } from 'next/navigation'
 import UserCard from '../components/UserCard'
 
 export default function ClientPage() {
-    const { data: session } = useSession({
-        required: true,
-        onUnauthenticated() {
-            redirect('/api/auth/signin?callbackUrl=/client')
-        }
-    })
+     const { data: session } = useSession()
+    // {
+    //     required: true,
+    //     onUnauthenticated() {
+    //         redirect('/api/auth/signin?callbackUrl=/client')
+    //     }
+    // })
+    if (session){
+        return(
+            <div>
+                <h1>Client</h1>
+                <p>You are successfully signedin to the client page</p>
+                <button onClick={() => signOut()}>Sign out</button>
+            </div>
+        )
+    }
 
     return (
         <section className="flex flex-col gap-6">
-            <UserCard  />
+            <h1>Client</h1>
+                <p>You are not signedin to the client page. Please click the button bellow to signin</p>
+                <button onClick={() => signIn()}>Sign In</button>
         </section>
     )
 }
