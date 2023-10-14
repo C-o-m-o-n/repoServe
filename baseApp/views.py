beaa24e0308da2be5dd0062012b04a9cb5c78d13
C-o-m-o-n <comon928@gmail.com>
2023-10-14 19:50:36 +0300

    first django commit

---
+++ baseApp/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+
+def login(request):
+    return render(request, 'login.html')
+
+@login_required
+def home(request):
+    return render(request, 'home.html')
