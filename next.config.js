2a5b1a11525d3b2a185b5f903a7ae498bbeda06c
Collins O. Odhiambo <comon928@gmail.com>
2023-10-14 12:42:15 +0000

    rename route

--- next.config.js
+++ next.config.js
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = {
-    ...nextConfig,
-    env:{
-        GITHUB_ID: process.env.GITHUB_ID,
-        GITHUB_SECRET: process.env.GITHUB_SECRET 
-    }
-}
+module.exports = nextConfig
